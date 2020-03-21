d7fd1b393aa20dc101b251e412c32a9f7acc6f98
Dmitriy Ionov <dim.ionow@yandex.ru>
2020-03-21 19:26:05 +0300

    remove extra lines

--- Lab10_Reactive/src/main/java/Main.java
+++ Lab10_Reactive/src/main/java/Main.java
@@ -2,7 +2,6 @@ import io.reactivex.netty.protocol.http.server.HttpServer;
 
 public class Main {
     public static void main(String[] args) {
-        // sudo systemctl start mongod
         CurrencyConverter converter = new CurrencyConverter();
         QueryHandler handler = new QueryHandlerImpl();
         HttpServer
